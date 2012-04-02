503ab43fae9102a10664a7890ad2907b05787be3
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-02 01:05:59 -0400

    replace fetching revision mapping with a hash, remove unused requires

--- lib/ruby_core_source.rb
+++ lib/ruby_core_source.rb
@@ -1,14 +1,14 @@
-
 require 'rbconfig'
-require 'tempfile'
-require 'tmpdir'
-require 'yaml'
-require File.join(File.dirname(__FILE__), 'contrib', 'uri_ext')
-require 'archive/tar/minitar'
-require 'zlib'
-require 'fileutils'
 
 module Ruby_core_source
+  REVISION_MAP = {
+    24186 => 'ruby-1.9.2-preview1',
+    27362 => 'ruby-1.9.2-preview3',
+    28524 => 'ruby-1.9.2-rc1',
+    28618 => 'ruby-1.9.2-rc2',
+    32789 => 'ruby-1.9.3-preview1',
+    33323 => 'ruby-1.9.3-rc1'
+  }
 
 def create_makefile_with_core(hdrs, name)
 
@@ -20,17 +20,11 @@ def create_makefile_with_core(hdrs, name)
     return true
   end
 
-  ruby_dir = ""
-  if RUBY_PATCHLEVEL < 0
-    Tempfile.open("preview-revision") { |temp|
-      uri = URI.parse("http://cloud.github.com/downloads/mark-moseley/ruby_core_source/preview_revision.yml")
-      uri.download(temp)
-      revision_map = YAML::load(File.open(temp.path))
-      ruby_dir = revision_map[RUBY_REVISION]
-      return false if ruby_dir.nil?
-    }
+  ruby_dir = if RUBY_PATCHLEVEL < 0
+    REVISION_MAP[RUBY_REVISION] or
+      no_source_abort("ruby-#{RUBY_VERSION} (revision #{RUBY_REVISION})")
   else
-    ruby_dir = "ruby-" + RUBY_VERSION.to_s + "-p" + RUBY_PATCHLEVEL.to_s
+    "ruby-#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
   end
 
   #
@@ -45,9 +39,7 @@ def create_makefile_with_core(hdrs, name)
   }
 
   dest_dir = File.dirname(__FILE__) + "/debugger/ruby_core_source/#{ruby_dir}"
-  unless File.directory?(dest_dir)
-    abort "No source for #{ruby_dir} provided with ruby_core_source gem."
-  end
+  no_source_abort(ruby_dir) unless File.directory?(dest_dir)
 
   with_cppflags("-I" + dest_dir) {
     if hdrs.call
@@ -58,5 +50,9 @@ def create_makefile_with_core(hdrs, name)
   return false
 end
 module_function :create_makefile_with_core
+
+def self.no_source_abort(ruby_version)
+  abort "No source for #{ruby_version} provided with debugger-ruby_core_source gem."
+end
 
 end
