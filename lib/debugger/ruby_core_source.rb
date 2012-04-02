42a3b47d29ac8fbe6f76daa43d0fe8ede01e1a9d
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-02 01:34:09 -0400

    add base require, version + rename/update gemspec

---
+++ lib/debugger/ruby_core_source.rb
@@ -0,0 +1,7 @@
+require 'ruby_core_source'
+require 'debugger/ruby_core_source/version'
+
+module Debugger
+  module RubyCoreSource
+  end
+end
---
+++ lib/debugger/ruby_core_source/version.rb
@@ -0,0 +1,5 @@
+module Debugger
+  module RubyCoreSource
+    VERSION = '1.0.0'
+  end
+end
